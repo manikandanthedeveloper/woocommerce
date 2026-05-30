6122f8e515673a7a8d4050e98b254f3df98d46e3
Manikandan Dhanapal <manikandanthedeveloper@users.noreply.github.com>
2026-05-30 14:47:46 +0530

    About us page

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
 				protocol: "https",
 				hostname: "skyblue-marten-522061.hostingersite.com",
 			},
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+			},
 		],
 	},
 };
